258e220542effe1559e81225b541412b559b200c
G00403720@atu.ie <G00403720@atu.ie>
2023-10-03 16:57:34 +0100

    hello

--- src/main/java/ie/atu/hello/Calculator.java
+++ src/main/java/ie/atu/hello/Calculator.java
@@ -1,13 +1,14 @@
 package ie.atu.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Calculator {
-    @GetMapping("/calculate")
-    public Answer calculate(@RequestParam int num1 , @RequestParam int num2 , @RequestParam String operation) {
+    @GetMapping("/calculate/{num1}/{num2}/{operation}")
+    public Answer calculate(@PathVariable int num1 , @PathVariable int num2 , @PathVariable String operation) {
         if (operation == "add"){
             return new Answer(operation,num1 + num2);
         }
